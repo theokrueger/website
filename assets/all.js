21ee67b2609f5100af49250a51e6851e335d3c25
theokrueger <theokrueger@airmail.cc>
2023-02-21 23:46:35 -0500

    fix font issues on smaller screen sizes

--- assets/all.js
+++ assets/all.js
@@ -1,21 +1,25 @@
 console.log('javascript enabled for this webpage');
-/* set flavour text on load*/
-var subtitles = [
-        'not actually a developer',
-        '$5 and your message here',
-        'anti-lua activist',
-        'button box enthusiast',
-        '.wav purist',
-        'datamania is real',
-        'ranked infinite dan',
-        'nmx d dot office',
-        'y2k compliant',
-        'commit victimless crimes',
-        'on the beatfloor',
-        'a true gakster',
-        'washing machine pro',
-        'jumping over lazy dogs',
-        'the dragon of mahjong',
-        'kaballa dolla heister',
-];
-document.getElementById("main-subtitle").innerHTML = subtitles[Math.floor(Math.random() * subtitles.length)];
+{
+        /* set flavour text on load*/
+        var subtitles = [
+                'not actually a developer',
+                '$5 and your message here',
+                'anti-lua activist',
+                'button box enthusiast',
+                '.wav purist',
+                'datamania is real',
+                'ranked infinite dan',
+                'nmx d dot office',
+                'y2k compliant',
+                'commit victimless crimes',
+                'on the beatfloor',
+                'a true gakster',
+                'washing machine pro',
+                'jumping over lazy dogs',
+                'the dragon of mahjong',
+                'kaballa dolla heister',
+        ];
+        if (document.getElementById("main-subtitle")) {
+                document.getElementById("main-subtitle").innerHTML = subtitles[Math.floor(Math.random() * subtitles.length)];
+        }
+}

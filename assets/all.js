face4873fac0ec73e622565ed1ed00e34362aa62
theokrueger <theokrueger@airmail.cc>
2023-04-08 12:53:52 -0400

    template migrate

--- assets/all.js
+++ assets/all.js
@@ -24,7 +24,7 @@ const idsToModify = {
                 'RTFM',
                 'man theokrueger.dev',
                 'tldr: website',
-                '',
+                'posix compliant',
         ],
 };
 

20537a0ed09c738f388ce8385e0c5d3310ded338
theokrueger <theokrueger@airmail.cc>
2023-04-08 11:51:10 -0400

    fix link spacing issue due to bad minifier conf

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 zip_name='page.zip'
 serve_cmd='python3 -m http.server'
 open_cmd='xdg-open http://localhost:8000'
-minify_dir_cmd="html-minifier-terser --case-sensitive --collapse-boolean-attributes --collapse-inline-tag-whitespace --collapse-whitespace --decode-entities --no-include-auto-generated-tags --keep-closing-slash --minify-css true --minify-js true --minify-urls true --remove-comments --remove-empty-attributes --remove-empty-elements --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --input-dir ./_build --output-dir ./_build"
+minify_dir_cmd="html-minifier-terser --case-sensitive --collapse-boolean-attributes --collapse-inline-tag-whitespace --collapse-whitespace --conservative-collapse --decode-entities --no-include-auto-generated-tags --keep-closing-slash --minify-css true --minify-js true --minify-urls true --remove-comments --remove-empty-attributes --remove-empty-elements --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --input-dir ./_build --output-dir ./_build"
 minify_html_cmd="$minify_dir_cmd --file-ext html"
 minify_css_cmd="$minify_dir_cmd --file-ext css"
 minify_js_cmd="$minify_dir_cmd --file-ext js"

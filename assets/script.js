b7c9a0394049fef186c1dd1f305161f21d9aaae4
theokrueger <theokrueger@airmail.cc>
2024-10-06 00:19:35 -0400

    add title flair chance

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@
 /// GPL-3.0 Licensed
 
 console.log('javascript enabled for this webpage');
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 /* Replace the flavour text with a random selection */
 const idsToModify = {
@@ -53,3 +54,36 @@ if (scrollProgressBox) {
     });
 }
 
+/* Fancy typing animation */
+const typingFlairs = [':)', ':D', ';)', ':]', ':3', ':O'];
+async function typeElement(elem, addRandomFlair) {
+    const txt = elem.innerHTML;
+    let len = elem.innerHTML.length;
+    const typingSpeed = 777 / len; // ms delay between chars
+    elem.innerHTML = "";
+    for (let i=0; i < len; i++) {
+	await sleep(typingSpeed);
+	elem.innerHTML += txt.charAt(i);
+    }
+
+    // 10% mhankce for random flair
+    if (addRandomFlair && Math.random() < 0.1) {
+	// type flair
+	await sleep(Math.random() * 5000 + 1000);
+	const flair = " " + typingFlairs[Math.floor(Math.random() * typingFlairs.length)];
+	for (let i=0; i < flair.length; i++) {
+	    await sleep(typingSpeed * 5);
+	    elem.innerHTML += flair.charAt(i);
+	}
+
+	// remove flair
+	await sleep(Math.random() * 5000 + 1000);
+	for (let i=flair.length; i >= 0; i--) {
+	    await sleep(typingSpeed * 3);
+	    elem.innerHTML = elem.innerHTML.substring(0, len + i);
+	}
+    }
+}
+
+/* type some elements */
+typeElement(document.getElementById("title-text"), true);

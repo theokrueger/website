db6938512179fd54146b2a4a82682999b7f30327
theokrueger <theokrueger@airmail.cc>
2025-10-31 17:47:52 -0400

    start robots lol

--- build.sh
+++ build.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
-rm -r ./_out
-mkdir ./_out
-ssgen --enable-shell --debug --input ./ --output ./_out
+# build and serve this webpage
+
+IN_DIR="./"
+OUT_F="./out.zip"
+SERVE_CMD="python3 -m http.server -d "
+
+#####
+
+die() {
+	echo "Failed!"
+	exit
+}
+
+TEMP_DIR="$(mktemp -d)"
+if [[ -f "$OUT_F" ]]; then
+        rm "$OUT_F"
+fi
+
+echo "Building Site..."
+ssgen --enable-shell --debug --input "$IN_DIR" --output "$TEMP_DIR" || die
+
+echo "Compressing Site..."
+zip "$OUT_F" "$TEMP_DIR"/* || die
+
+if [[ "$1" == "serve" ]]; then
+	echo "Serving Site..."
+        $SERVE_CMD "$TEMP_DIR" || die
+fi
+
+echo "Cleaning up..."
+rm -rf "$TEMP_DIR" || die
--- post/2024/robots/robots.js
+++ post/2024/robots/robots.js
@@ -2,12 +2,19 @@
 
 /* Util */
 const blurElem = document.getElementById("blur-all");
+var blurCount = 0;
 async function blur(blurAmt = "15px") {
-  blurElem.style.backdropFilter = "blur(" + blurAmt + ") grayscale(15%)";
-  animateFadeIn(blurElem, "block");
+  if (blurCount <= 0) {
+    blurElem.style.backdropFilter = "blur(" + blurAmt + ") grayscale(15%)";
+    animateFadeIn(blurElem, "block");
+  }
+  ++blurCount;
 }
 async function unblur() {
-  animateFadeOut(blurElem, "none");
+  --blurCount;
+  if (blurCount <= 0) {
+    animateFadeOut(blurElem, "none");
+  }
 }
 
 function getOffset(elem) {
@@ -57,17 +64,22 @@ async function animateSlideIn(
   elem,
   display,
   direction,
-  animationTimeMs = 100,
-  animationSteps = 25,
+  animationTimeMs = 200,
+  animationSteps = 50,
 ) {
   const stepTime = animationTimeMs / animationSteps;
-  elem.style.opacity = "0%";
+  const oldpos = elem.style.position;
+  const oldright = elem.style.right;
+  const width = elem.style.width;
   elem.style.display = display;
+  elem.style.position = "relative";
   for (let i = 0; i < animationSteps; ++i) {
-    elem.style.opacity = ((i * 100) / animationSteps).toString() + "%";
+    elem.style.right =
+      "calc(-100% + " + ((i * 100) / animationSteps).toString() + "%)";
     await sleep(stepTime);
   }
-  elem.style.opacity = "100%";
+  elem.style.right = oldright;
+  elem.style.position = oldpos;
 }
 
 /* User pestering */
@@ -94,12 +106,12 @@ var annoyances = [
     const subscribeNewsletterButton = document.getElementById(
       "subscribe-newsletter",
     );
-    await animateSlideIn(newsletterElem, "block", "N");
+    await animateFadeIn(newsletterElem, "block", "N");
 
     subscribeNewsletterButton.addEventListener("click", async function () {
       newsletterElem.innerHTML =
         "<p>Thanks for subscribing! Expect 400,000 emails shortly</p>";
-      await sleep(2000);
+      await sleep(3000);
       animateFadeOut(newsletterElem);
     });
 
@@ -111,48 +123,111 @@ var annoyances = [
   async function () {
     const newsletterElem = document.getElementById("notifications");
     newsletterElem.style.display = "block";
+    const n = 12;
+    let remaining = [];
+    for (let i = 0; i < n; ++i) {
+      remaining.push(i);
+    }
 
-    for (let i = 0; i < 6; ++i) {
+    for (let i = 0; i < n; ++i) {
+      const ri = Math.floor(Math.random() * remaining.length);
+      const item = remaining[ri];
+      remaining.splice(ri, 1);
       const notification = document.getElementById(
-        "notification-" + i.toString(),
+        "notification-" + item.toString(),
       );
       const notificationButton = document.getElementById(
-        "close-notification-" + i.toString(),
+        "close-notification-" + item.toString(),
       );
       notificationButton.addEventListener("click", async function () {
         animateFadeOut(notification);
       });
       await animateSlideIn(notification, "block", "W");
-      await sleep(Math.random() * 30000 + 5000);
+      // 7-30s between notifs
+      await sleep(Math.random() * 23000 + 7000);
     }
+  },
+  // Paywall
+  async function () {
+    const paywallElem = document.getElementById("paywall");
+
+    const closePaywallButton = document.getElementById("close-paywall");
+    const continuePaywallButton = document.getElementById("continue-paywall");
+    const paywallEmailField = document.getElementById("paywall-email");
+    const paywallErrorText = document.getElementById("paywall-error");
+
+    blur();
+    await animateFadeIn(paywallElem, "block");
+
+    const closePaywallFunc = async function () {
+      animateFadeOut(paywallElem);
+      await unblur();
+    };
+
+    closePaywallButton.addEventListener("click", closePaywallFunc);
+
+    continuePaywallButton.addEventListener("click", async function () {
+      if (paywallEmailField.value === "") {
+        paywallErrorText.innerHTML = "Please enter your email!";
+        return;
+      }
+      paywallErrorText.innerHTML = "";
+
+      let t = [
+        "Contacting Server",
+        "Loading Account",
+        "Fingerprinting Browser",
+        "Downloading Subscription Status",
+        "Loading Breadcrumbs",
+        "Searching Financial Records",
+        "Generating B 101A Form",
+        "Finalizing",
+      ];
+      for (let i = 0; i < t.length; ++i) {
+        continuePaywallButton.innerHTML = t[i];
+        await sleep(500);
+        for (let j = 0; j < 5; ++j) {
+          continuePaywallButton.innerHTML += ".";
+          await sleep(300 + 100 * Math.random());
+          if (Math.random() > 0.9 - j * 0.1) {
+            break;
+          }
+        }
+      }
+
+      continuePaywallButton.innerHTML =
+        "Welcome, " + paywallEmailField.value.split("@")[0] + "!";
+      await sleep(2000);
+      closePaywallFunc();
+    });
   },
 ];
 
 async function annoyUser() {
-  // await sleep(Math.random() * 60000 + 20000);
-  // // run a random annoyance every 20-60 seconds
-  // while (annoyances.length > 0) {
-  //   const i = Math.floor(Math.random() * annoyances.length);
-  //   annoyances[i]();
-  //   annoyances.splice(i, 1);
-  //   await sleep(Math.random() * 45000 + 15000);
-  // }
+  await sleep(Math.random() * 7000 + 2000);
+  // run a random annoyance every 20-40 seconds
+  while (annoyances.length > 0) {
+    const i = Math.floor(Math.random() * annoyances.length);
+    annoyances[i]();
+    annoyances.splice(i, 1);
+    await sleep(Math.random() * 20000 + 20000);
+  }
   // test anti-adblocker
   //annoyances[0]();
   // test newsletter
-  // annoyances[1]();
+  //annoyances[1]();
   // test notifications
-  annoyances[2]();
+  //annoyances[2]();
+  // test paywall
+  //annoyances[3]();
 }
 
 // IDEAS (not yet implemented)
 // spin a wheel
-// notifications
 // ask for permissons
-// captcha
+// captchax
 // account
 // hide the article
-// paywall
 // install our desktop app
 // unsupported browser
 // contact live chat
